380a98ce81830599e6a72cee28303c19a61b0ac8
RayYu-ChunleiYu <Ray.yu@whu.edu.cn>
2023-06-20 19:52:05 +0800

    treewidget get inputs

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from .Database import Database
-from .Models import Experiment,Source,Specimen,Steel,Concrete,Geometry,Measurement
+from .Models import Experiment,Source,Specimen,Steel,Concrete,Geometry,Measurement,MyMagic
 from sqlalchemy import and_, or_
